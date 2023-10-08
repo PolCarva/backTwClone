79aeb7126bd49a34ba0bbc3b2556f14d4faa1117
lucas lorenzo <luquitas02@gmail.com>
2023-10-08 12:32:23 -0300

    dfdf

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,8 +4,8 @@ const logger = require('./logger');
 const sendEmail = async(from, to, subject, message) => {
 	const transporter = nodemailer.createTransport({
 		host: process.env.EMAIL_HOST,
-		port: 465 ,
-		secure: false,
+		port: 587,
+		secure: true,
 		secureOptions:{
 			ssl: 'TLSv1_2_method'
 		},
@@ -14,10 +14,25 @@ const sendEmail = async(from, to, subject, message) => {
 			pass: process.env.EMAIL_PASS
 		},
 		tls:{
-			rejectUnauthorized: false,
-			ciphers: 'TLSv1.2'
+			rejectUnauthorized: true,
+			ciphers: 'SSLv3'
 		}
 	});
+
+
+	await new Promise((resolve, reject) => {
+		// verify connection configuration
+		transporter.verify(function (error, success) {
+			if (error) {
+				console.log(error);
+				reject(error);
+			} else {
+				console.log('Server is ready to take our messages');
+				resolve(success);
+			}
+		});
+	});
+
 
 	const options = {
 		from,
@@ -26,12 +41,17 @@ const sendEmail = async(from, to, subject, message) => {
 		html: message
 	};
 
-	transporter.sendMail(options, (err, info) => {
-		if (err) {
-			logger.info('hubo un error ' + err);
-		} else {
-			logger.info(info,'mail enviado');
-		}
+	await new Promise((resolve, reject) => {
+		// send mail
+		transporter.sendMail(options, (err, info) => {
+			if (err) {
+				console.error(err);
+				reject(err);
+			} else {
+				console.log(info);
+				resolve(info);
+			}
+		});
 	});
 };
 
