2225aaf84d3d30d9defeec5f04554b801037b0b0
lucas lorenzo <luquitas02@gmail.com>
2023-10-09 11:19:01 -0300

    asdas

---
+++ src/public/main.js
@@ -0,0 +1,35 @@
+const socket = io.connect('https://socialmediaclone-production-1e63.up.railway.app',{
+	auth: {
+		token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjk2ODYxMDQwfQ.7XxePWbJSrfFS6NsCMN-NzHCUpHEjIQ_BZuFNfIEOp8'
+	}
+});
+
+let userData = null;
+
+socket.on('user', (user) => {
+	userData = user;
+});
+
+let chatId = 1;
+
+socket.emit('join chat', chatId);
+
+socket.on('get messages', (messages)=> {
+	console.log(messages);
+});
+
+
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	if (input.value && userData) {
+		socket.emit('send message', input.value, userData.id, 1);
+		input.value = '';
+	}
+});
+
+socket.on('get new message', (messages)=> {
+	console.log(messages);
+}); 
