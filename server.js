0925f73200e54d48498602aa2979a61db7294df1
lucas lorenzo <luquitas02@gmail.com>
2023-10-20 11:50:58 -0300

    dssdadassda

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ async function server (){
 		});
 	}else{ */
 
-	await sequelize.sync({force: true}).then(() => {
+	await sequelize.sync({alter: true}).then(() => {
 		logger.info('All models were synchronized successfully.');
 	}).catch((err) => {
 		logger.info(err);
