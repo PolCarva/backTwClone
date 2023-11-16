9a5339718f7182852963e05770429f121326b59a
PolCarva <pablocarvalhogimenez@gmail.com>
2023-11-16 11:18:55 -0300

    User photo update bug fixing

--- src/database/users.js
+++ src/database/users.js
@@ -1,79 +1,79 @@
-const User = require('../models/user');
-const logger = require('../utils/logger');
-const IncludeOptions = require('./includeOptions');
+const User = require("../models/user");
+const logger = require("../utils/logger");
+const IncludeOptions = require("./includeOptions");
 
-class UsersDAO{
-	constructor(){
-		this.includeOptions = new IncludeOptions;
-	}
+class UsersDAO {
+  constructor() {
+    this.includeOptions = new IncludeOptions();
+  }
 
-	async createUser(newUser){
-		try{
-			return User.create(newUser);
-		}catch(err){
-			logger.info(err);
-		}
-	}
+  async createUser(newUser) {
+    try {
+      return User.create(newUser);
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 
-	async getUserByUsername(username){
-		try{
-			return User.findOne({ 
-				where: { 
-					username 
-				} 
-			});
-		}catch(err){
-			logger.info(err);
-		}
-	}
+  async getUserByUsername(username) {
+    try {
+      return User.findOne({
+        where: {
+          username,
+        },
+      });
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 
-	async getUserByMail(email){
-		try{
-			return User.findOne({ 
-				where: { 
-					email 
-				} 
-			});
-		}catch(err){
-			logger.info(err);
-		}
-	}
+  async getUserByMail(email) {
+    try {
+      return User.findOne({
+        where: {
+          email,
+        },
+      });
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 
-	async getUserById(id){
-		try{
-			return User.findOne({ 
-				where: { 
-					id 
-				}, include: this.includeOptions.getUserIncludeOptions()
-			});
-		}catch(err){
-			logger.info(err);
-		}
-	}
+  async getUserById(id) {
+    try {
+      return User.findOne({
+        where: {
+          id,
+        },
+        include: this.includeOptions.getUserIncludeOptions(),
+      });
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 
-	async getUsersById(id){
-		try{
-			return User.findAll({ 
-				where: { 
-					id 
-				}
-			}
-			);
-		}catch(err){
-			logger.info(err);
-		}
-	}
+  async getUsersById(id) {
+    try {
+      return User.findAll({
+        where: {
+          id,
+        },
+      });
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 
-	async getAllUsers(){
-		try{
-			return User.findAll({
-				attributes: ['username', 'id', 'full_name', 'profile_photo']
-			});
-		}catch(err){
-			logger.info(err);
-		}
-	}
-	/* 
+  async getAllUsers() {
+    try {
+      return User.findAll({
+        attributes: ["username", "id", "full_name", "profile_photo"],
+      });
+    } catch (err) {
+      logger.info(err);
+    }
+  }
+  /* 
     async updateUserChats(userId, chatId){
         try{
             return await User.findByIdAndUpdate(userId, {$push :{chats: chatId}})
@@ -82,63 +82,73 @@ class UsersDAO{
         }
     } */
 
-	async updateUserPassword(userId, newPassword){
-		try{
-			return User.update({password: newPassword},{
-				where:{
-					id: userId
-				}
-			}
-			);
-		}catch(err){
-			logger.info(err);
-		}
-	}
+  async updateUserPassword(userId, newPassword) {
+    try {
+      return User.update(
+        { password: newPassword },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 
-	async updateUserData(userId, username, fullName, profilePhoto, bio, dayOfBirth){
-		try{
-			return User.update({username, full_name: fullName, profile_photo: profilePhoto, bio, day_of_birth: dayOfBirth}, {
-				where: {
-					id: userId
-				}
-			});
-		}catch(err){
-			logger.info(err);
-		}
-	}
+  async updateUserData(userId, username, fullName, profilePhoto, bio) {
+    try {
+      return User.update(
+        { username, full_name: fullName, profile_photo: profilePhoto, bio },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 
-	async updateUserStatus(userId, onlineValue){
-		try{
-			return await User.update(
-				{online: onlineValue}, {
-					where: {
-						id: userId
-					}
-				});
-		}catch(err){
-			logger.info(err);
-		}
-	}
+  async updateUserStatus(userId, onlineValue) {
+    try {
+      return await User.update(
+        { online: onlineValue },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 
-	async deleteUser(userId){
-		try{
-			return User.destroy({id: userId});
-		}catch(err){
-			logger.info(err);
-		}
-	}
+  async deleteUser(userId) {
+    try {
+      return User.destroy({ id: userId });
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 
-	async activateUser(userId){
-		try {
-			return User.update({activated: true}, {
-				where:{
-					id: userId
-				}
-			});
-		} catch (err) {
-			logger.info(err);
-		}
-	}
+  async activateUser(userId) {
+    try {
+      return User.update(
+        { activated: true },
+        {
+          where: {
+            id: userId,
+          },
+        }
+      );
+    } catch (err) {
+      logger.info(err);
+    }
+  }
 }
 
 module.exports = UsersDAO;
--- src/services/users.js
+++ src/services/users.js
@@ -1,46 +1,61 @@
-const UsersDAO = require('../database/users');
-const {uploadFile, readFile} = require('../utils/awsS3');
+const UsersDAO = require("../database/users");
+const { uploadFile, readFile } = require("../utils/awsS3");
 
-class UsersApi{
-	constructor(){
-		this.usersDAO = new UsersDAO();
-	}
-    
-	async getAllUsers(){
-		return await this.usersDAO.getAllUsers();
-	}
+class UsersApi {
+  constructor() {
+    this.usersDAO = new UsersDAO();
+  }
 
-	async getUserByUsername(username){
-		return await this.usersDAO.getUserByUsername(username);
-	}
+  async getAllUsers() {
+    return await this.usersDAO.getAllUsers();
+  }
 
-	async getUserById(id){
-		return await this.usersDAO.getUserById(id);
-	}
+  async getUserByUsername(username) {
+    return await this.usersDAO.getUserByUsername(username);
+  }
 
-	async getUsersById(id){
-		return await this.usersDAO.getUsersById(id);
-	}
+  async getUserById(id) {
+    return await this.usersDAO.getUserById(id);
+  }
 
-	async updateUserChats(userId, chatId){
-	}
+  async getUsersById(id) {
+    return await this.usersDAO.getUsersById(id);
+  }
 
-	async updateUserData(profilePhotoFile, fileName, userId, username, fullName, bio, dayOfBirth, fileUrl){
-		if(profilePhotoFile && fileName && fileUrl){
-			await uploadFile(profilePhotoFile, fileName);
-		}
+  async updateUserChats(userId, chatId) {}
 
-		await this.usersDAO.updateUserData(userId, username, fullName, fileUrl, bio, dayOfBirth);
-		return await this.usersDAO.getUserById(userId);
-	}
+  async updateUserData(
+    profilePhotoFile,
+    fileName,
+    userId,
+    username,
+    bio,
+    fullName,
+    fileUrl
+  ) {
+    const user = await this.getUserById(userId);
 
-	async updateUserStatus(userId, online){
-		return await this.usersDAO.updateUserStatus(userId, online);
-	}
+    if (profilePhotoFile && fileName && fileUrl) {
+      await uploadFile(profilePhotoFile, fileName);
+    }
 
-	async deleteUser(userId){
-		return await this.usersDAO.deleteUser(userId);
-	}
+    await this.usersDAO.updateUserData(
+      userId,
+      username || user.dataValues.username,
+      fullName || user.dataValues.full_name,
+      fileUrl || user.dataValues.profile_photo,
+      bio || user.dataValues.bio
+    );
+    return user;
+  }
+
+  async updateUserStatus(userId, online) {
+    return await this.usersDAO.updateUserStatus(userId, online);
+  }
+
+  async deleteUser(userId) {
+    return await this.usersDAO.deleteUser(userId);
+  }
 }
 
 module.exports = UsersApi;
