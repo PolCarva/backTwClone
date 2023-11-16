c166f4a17d7c54c4fa3ea22d9f96bbc4e418518b
PolCarva <pablocarvalhogimenez@gmail.com>
2023-11-16 12:19:50 -0300

    Photo persistance 2

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,52 +1,59 @@
-const asyncHandler = require('express-async-handler');
-const UsersApi = require('../services/users');
+const asyncHandler = require("express-async-handler");
+const UsersApi = require("../services/users");
 
-class UsersController{
-	constructor(){
-		this.usersApi = new UsersApi();
-	}
-	//SI NO PUEDO VISUALIZAR LA FOTO DE PERFIL, REVISAR EL BUCKET Y DESTILDAR LA OPCION DE BLOQUEAR ACCESO
-	updateUserData = asyncHandler(async(req, res) => {
-		try {
-			const{username, fullName, bio} = req.body;
-			const{id} = req.user;
-			const newUser = await this.usersApi.updateUserData(req.files?.file.tempFilePath, 
-				req.files?.file.name, id, username || req.user.username, bio || req.user.bio, fullName || req.user.full_name, 
-				https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${req.files?.file.name});
-			console.log(newUser);
-			res.json({success: true, message: 'usuario actualizado', newUser}).status(200);
-		} catch (err) {
-			res.json({success: false, message: err.message}).status(500);
-		}
-	});
+class UsersController {
+  constructor() {
+    this.usersApi = new UsersApi();
+  }
+  //SI NO PUEDO VISUALIZAR LA FOTO DE PERFIL, REVISAR EL BUCKET Y DESTILDAR LA OPCION DE BLOQUEAR ACCESO
+  updateUserData = asyncHandler(async (req, res) => {
+    try {
+      const { username, fullName, bio } = req.body;
+      const { id } = req.user;
+      const newUser = await this.usersApi.updateUserData(
+        req.files?.file.tempFilePath,
+        req.files?.file.name,
+        id,
+        username || req.user.username,
+        bio || req.user.bio,
+        fullName || req.user.full_name,
+        `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${req.files?.file.name}`
+      );
+      console.log(newUser);
+      res
+        .json({ success: true, message: "usuario actualizado", newUser })
+        .status(200);
+    } catch (err) {
+      res.json({ success: false, message: err.message }).status(500);
+    }
+  });
 
-	getMyProfile = asyncHandler(async(req, res) => {
-		try {
-			const user = await this.usersApi.getUserById(req.user.id);
-			res.json({success: true, data: user}).status(200);
-		} catch (err) {
-			res.json({success: false, message: err}).status(500);
-		}
-	});
+  getMyProfile = asyncHandler(async (req, res) => {
+    try {
+      const user = await this.usersApi.getUserById(req.user.id);
+      res.json({ success: true, data: user }).status(200);
+    } catch (err) {
+      res.json({ success: false, message: err }).status(500);
+    }
+  });
 
-	getUserProfile = asyncHandler(async(req, res) => {
-		try {
-			const user = await this.usersApi.getUserById(req.params.userid);
-			res.json({success: true, data: user}).status(200);
-		} catch (err) {
-			res.json({success: false, message: err}).status(500);
-		}
-	});
+  getUserProfile = asyncHandler(async (req, res) => {
+    try {
+      const user = await this.usersApi.getUserById(req.params.userid);
+      res.json({ success: true, data: user }).status(200);
+    } catch (err) {
+      res.json({ success: false, message: err }).status(500);
+    }
+  });
 
-	getAllUsers = asyncHandler(async(req, res) => {
-		try {
-			const users = await this.usersApi.getAllUsers();
-			res.json({success: true, data: users}).status(200);
-		} catch (err) {
-			res.json({success: false, message: err}).status(500);
-		}
-	});
-
+  getAllUsers = asyncHandler(async (req, res) => {
+    try {
+      const users = await this.usersApi.getAllUsers();
+      res.json({ success: true, data: users }).status(200);
+    } catch (err) {
+      res.json({ success: false, message: err }).status(500);
+    }
+  });
 }
 
 module.exports = UsersController;
